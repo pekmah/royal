2e293513833eeecca6fe813ab84d2ecaac7f3b4e
Danny Wamuya <dwamuya@gmail.com>
2023-06-09 14:03:43 +0300

    feat: implement auth routing

---
+++ app/[auth]/password_reset/page.tsx
@@ -0,0 +1,3 @@
+export default function PasswordReset() {
+  return <main className="">Password Reset</main>;
+}
---
+++ app/[auth]/signup/page.tsx
@@ -0,0 +1,3 @@
+export default function Signup() {
+  return <main className="">Signup</main>;
+}
