163b8599a74ddd76ffa90ec2bea1b122530896e1
Danny Wamuya <dwamuya@gmail.com>
2023-06-14 13:33:22 +0300

    feat: updated signup to use nextjs api routes

---
+++ app/api/auth/signup/route.ts
@@ -0,0 +1,13 @@
+import { postRequest } from '@/utils/request';
+
+export async function POST(request: Request) {
+	const data = await request.json();
+	try {
+		const res = await postRequest('/api/v1/auth/user/register/', data);
+		return new Response(JSON.stringify(res.data));
+	} catch (e: any) {
+		return new Response(JSON.stringify(e.response.data), {
+			status: e.response.status,
+		});
+	}
+}
