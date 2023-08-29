b029962345619be7d01d747cea494c2ec60bcdab
pekmah <ericpekmah@gmail.com>
2023-08-29 12:41:05 +0300

    feat(#50): whatsapp button create

---
+++ components/WhatsappBtn.js
@@ -0,0 +1,32 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+const WhatsAppButton = () => {
+  const handleWhatsAppButtonClick = () => {
+    window.open("https://wa.me/254703567890", "_blank");
+  };
+
+  return (
+    <div
+      className={"shadow"}
+      style={{
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        color: "#fff",
+        borderRadius: "50%",
+        background: "#25d366",
+        width: "55px",
+        height: "55px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        cursor: "pointer",
+      }}
+      onClick={handleWhatsAppButtonClick}
+    >
+      <FaWhatsapp className="text-white" style={{ fontSize: "38px" }} />
+    </div>
+  );
+};
+
+export default WhatsAppButton;
