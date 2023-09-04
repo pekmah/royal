fdc272307376a77c251c9e3f77f170bd89b15312
pekmah <ericpekmah@gmail.com>
2023-09-04 06:50:32 +0300

    feat(#71-carousel): carousel section created

---
+++ components/About/Carousel.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Image from "next/image";
+import bgImage from "../../public/about-img.jpg";
+
+const Carousel = () => {
+  return (
+    <div className={"h-[600px] bg-slate-400 w-full flex relative"}>
+      <Image
+        className={"h-full flex-1 object-cover bg-no-repeat"}
+        src={bgImage}
+        alt={"bg-image"}
+      />
+
+      <div
+        className={
+          "absolute bg-[#00000040] top-0 left-0 right-0 bottom-0 flex items-center px-10"
+        }
+      >
+        <h3
+          className={
+            "xl:w-3/5 w-3/4 text-white font-[600] text-[32px] leading-[60px]"
+          }
+        >
+          We do more than just roofing. We provide the guarantee of safety
+        </h3>
+      </div>
+
+      <div
+        className={
+          "flex bg-white absolute -bottom-10 px-10 right-[10vw] left-[10vw] py-6 z-20 rounded-xl shadow justify-around"
+        }
+      >
+        <div
+          className={"flex flex-col items-center justify-center text-center"}
+        >
+          <h3 className={"font-[700] text-[28px] font-inter text-primary_red"}>
+            3.5k +
+          </h3>
+          <h6 className={"text-lg font-inter font-[500] text-black"}>
+            Houses roofed
+          </h6>
+        </div>
+
+        <div
+          className={"flex flex-col items-center justify-center text-center"}
+        >
+          <h3 className={"font-[700] text-[28px] font-inter text-primary_red"}>
+            864 +
+          </h3>
+          <h6 className={"text-lg font-inter font-[500] text-black"}>
+            Quotes Processed
+          </h6>
+        </div>
+
+        <div
+          className={"flex flex-col items-center justify-center text-center"}
+        >
+          <h3 className={"font-[700] text-[28px] font-inter text-primary_red"}>
+            2.5K +
+          </h3>
+          <h6 className={"text-lg font-inter font-[500] text-black"}>
+            Deliveries made
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
--- components/PageSections/MainContainer.tsx
+++ components/PageSections/MainContainer.tsx
@@ -24,28 +24,31 @@ export default function MainContainer({ children }: { children: ReactNode }) {
         setIsSidebarOpen={setIsSidebarOpen}
         isSidebarOpen={isSidebarOpen}
       />
-
-      <main className={`w-screen p-8 flex gap-8 bg-[#fbfbff]`}>
-        {!path.startsWith("/checkout") && !path.startsWith("/account") && (
-          <Sidebar isOpen={isSidebarOpen} />
-        )}
-        {!path.startsWith("/auth") &&
-        !path.startsWith("/about") &&
-        !path.startsWith("/account") ? (
-          <section
-            className={`${
-              isSidebarOpen ? "md:w-[80%] hidden md:block" : "w-full"
-            } transition-all mt-32 md:mt-[0] duration-300`}
-          >
-            {children}
-          </section>
-        ) : (
-          <section className="w-full transition-all ease-in-out duration-300">
-            {children}
-          </section>
-        )}
-        <WhatsappBtn />
-      </main>
+      {path.startsWith("/about") ? (
+        <main className={`w-screen bg-[#fbfbff]`}>{children}</main>
+      ) : (
+        <main className={`w-screen p-8 flex gap-8 bg-[#fbfbff]`}>
+          {!path.startsWith("/checkout") && !path.startsWith("/account") && (
+            <Sidebar isOpen={isSidebarOpen} />
+          )}
+          {!path.startsWith("/auth") &&
+          !path.startsWith("/about") &&
+          !path.startsWith("/account") ? (
+            <section
+              className={`${
+                isSidebarOpen ? "md:w-[80%] hidden md:block" : "w-full"
+              } transition-all mt-32 md:mt-[0] duration-300`}
+            >
+              {children}
+            </section>
+          ) : (
+            <section className="w-full transition-all ease-in-out duration-300">
+              {children}
+            </section>
+          )}
+          <WhatsappBtn />
+        </main>
+      )}
       <Footer />
     </>
   );
