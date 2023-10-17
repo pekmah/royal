a865939902514dda14186de451baa94ae5dc02b8
Pekmah <ericpekmah@gmail.com>
2023-10-17 08:41:56 +0300

    feat: Privacy page added

---
+++ app/about/privacy/page.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <div className={"md:mx-28"}>
+      <h3 className={"mt-5 text-center font-barlow text-[30px] font-[600]"}>
+        PRIVACY STATEMENT
+      </h3>
+      <span className={"leading-8 font-barlow text-lg"}>
+        Our privacy statement...
+      </span>
+      <br />
+      <br />
+      <p className=" leading-8 font-barlow text-[#333333] font-[400] pb-6">
+        Royal Mabati Factory Ltd does not sell, rent, or lease any of its
+        customer information to third parties and will not do so in the future.
+        Royal Mabati Factory may also contact you about completing surveys
+        and/or research questionnaires about your experience with our products.
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
