fe48501b7bf94152880631e1948265ccac508a9e
Pekmah <ericpekmah@gmail.com>
2023-10-17 09:53:24 +0300

    feat(#117): dropdown icon added

---
+++ public/svg/ArrowDropDownSvg.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ArrowDropDownSvg = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.0001 15L7.75708 10.758L9.17208 9.34399L12.0001 12.172L14.8281 9.34399L16.2431 10.758L12.0001 15.001V15Z"
+        fill="#2C3699"
+      />
+    </svg>
+  );
+};
+
+export default ArrowDropDownSvg;
