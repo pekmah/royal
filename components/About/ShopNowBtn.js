00ee3997c0ec53ed2c6623bd083ffa4dd66d8a98
Pekmah <ericpekmah@gmail.com>
2023-10-17 21:48:50 +0300

    refactor(#117): button hidden on small screens

--- components/About/ShopNowBtn.js
+++ components/About/ShopNowBtn.js
@@ -4,7 +4,7 @@ const ShopNowBtn = () => {
   return (
     <button
       className={
-        "px-5 py-1 my-1 rounded-lg text-white font-semibold gradient-bg ml-2"
+        "hidden md:flex px-5 py-1 items-center  my-1 rounded-lg text-white font-semibold gradient-bg ml-2"
       }
     >
       Shop Now
